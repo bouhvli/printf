dfd2885824c1c5cf5b3c61734715830aae81280d
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-26 01:00:35 -0700

    refactor bin function

--- adv_func.c
+++ adv_func.c
@@ -9,22 +9,36 @@
  */
 int print_b(va_list args)
 {
-	unsigned int val, binnum[32];
-	int count, j, i;
+	unsigned int num;
+	int count = 0;
+	int highest_bit, i, bit;
 
-	val = va_arg(args, unsigned int);
-	count = 0;
-	if (val == NULL)
-		return (0);
-	for (i = 0; val > 0; i++)
+	num = va_arg(args, unsigned int);
+	if (num == 0)
 	{
-		binnum[i] = val % 2;
-		val /= 2;
-	}
-	for (j = i - 1; j >= 0; j--)
-	{
-		_putchar(binnum[j] + '0');
+		_putchar('0');
 		count++;
+	}
+	else
+	{
+		/**
+		 * Determine the highest non-zero
+		 * bit in the input number
+		 */
+		highest_bit = 31;
+		while (((num >> highest_bit) & 1) == 0 && highest_bit > 0)
+			highest_bit--;
+
+		/**
+		 * Print the binary representation
+		 * of the input number
+		 */
+		for (i = highest_bit; i >= 0; i--)
+		{
+			bit = (num >> i) & 1;
+			_putchar(bit + '0');
+			count++;
+		}
 	}
 	return (count);
 }
